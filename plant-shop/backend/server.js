b21940bae13d2e842817ac4f55cd8bc642391a30
022Nichapha <66429022@webmail.npru.ac.th>
2025-10-01 01:13:17 +0700

    Updaetbackend

--- plant-shop/backend/server.js
+++ plant-shop/backend/server.js
@@ -1,3 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Serve frontend build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
